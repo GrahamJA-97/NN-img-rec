3b5d72ad656e0a6cbde5c907e9f897ef5fc6d6bb
graha4ja <graha4ja@dukes.jmu.edu>
2019-10-15 17:25:37 -0400

    added the model.py files

---
+++ m1.py
@@ -0,0 +1,54 @@
+#    name: pa2Template.py
+# purpose: template for building a Keras model
+#          for hand written number classification
+#    NOTE: Submit a different python file for each model
+# -------------------------------------------------
+
+
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation
+from keras.optimizers import SGD
+from keras.utils import np_utils
+from keras.models import load_model
+
+from pa2pre import processTestData
+import argparse
+
+
+def parseArguments():
+    parser = argparse.ArgumentParser(
+        description='Build a Keras model for Image classification')
+
+    parser.add_argument('--training_x', action='store',
+                        dest='XFile', default="", required=True,
+                        help='matrix of training images in npy')
+    parser.add_argument('--training_y', action='store',
+                        dest='yFile', default="", required=True,
+                        help='labels for training set')
+
+    parser.add_argument('--outModelFile', action='store',
+                        dest='outModelFile', default="", required=True,
+                        help='model name for your Keras model')
+
+    return parser.parse_args()
+
+def main():
+    np.random.seed(1671)
+
+    parms = parseArguments()
+
+    X_train = np.load(parms.XFile)
+    y_train = np.load(parms.yFile)
+
+    (X_train, y_train) = processTestData(X_train,y_train)
+
+    print('KERA modeling build starting...')
+    ## Build your model here
+
+    ## save your model
+
+
+if __name__ == '__main__':
+    main()
